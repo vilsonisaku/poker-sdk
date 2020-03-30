7d1e9d717f9c28101940cf9dd4d568c2c7b53d00
vilson <vilsonisaku93@gmail.com>
2020-03-30 22:11:51 +0200

    added QueueKeys constants

---
+++ app/QueueKeys.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ExHelp;
+
+
+class QueueKeys
+{
+    const main = "default";
+
+    const backoffice_shared = "backoffice_shared";
+
+    const caching_shared = "caching_shared";
+
+    const account = "account";
+
+    const websocket = "websocket";
+
+}
