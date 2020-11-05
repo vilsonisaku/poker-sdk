a266cecb93a65ccddcab0f677d3e53f2ed1c485b
vilson <vilsonisaku93@gmail.com>
2020-11-05 12:05:57 +0100

    updated

--- app/Provider.php
+++ app/Provider.php
@@ -7,6 +7,7 @@ class Provider {
 
     const SKINS = 'api/skins';
     const CURRENCIES = 'api/currencies';
+    const CASINO = 'api/skin';
 
     protected $endpoint;
     protected $token;
@@ -22,3 +23,39 @@ class Provider {
             'Authorization'=>'Bearer '.$this->token,
         ];
     }
+
+
+
+    /*
+    *   Get list of skins
+    */
+    public function getSkins(){
+
+        $url = $this->endpoint.static::SKINS;
+
+        $response = (new Client)->get( $url , [
+            'headers'=> $this->getHeaders()
+        ]);
+
+        return json_decode($response->getBody()->getContents());
+    }
+
+
+    /*
+    *   Get skin id by domain
+    */
+    public function getSkinIdByDomain($domain){
+
+        $url = $this->endpoint.static::CASINO;
+
+        $response = (new Client)->get( $url , [
+            'query' => [
+                'domain' => $domain,
+            ],
+            'headers'=> $this->getHeaders()
+        ]);
+
+        return json_decode($response->getBody()->getContents());
+    }
+
+}
